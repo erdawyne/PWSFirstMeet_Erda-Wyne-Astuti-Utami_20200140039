bec4b55d51dcb413e5b52e626c057f57921d25d8
ERDA WYNE <erdawynee1@gmail.com>
2022-10-18 00:13:28 +0700

    menambahkan file from zip

---
+++ src/main/java/a/ws/invetaris/InvetarisApplication.java
@@ -0,0 +1,13 @@
+package a.ws.invetaris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvetarisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvetarisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/a/ws/invetaris/InvetarisApplicationTests.java
@@ -0,0 +1,13 @@
+package a.ws.invetaris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvetarisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
