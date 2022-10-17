11c5d800f8b9017e7da953aa57c5db7b7628b01e
ERDA WYNE <erdawynee1@gmail.com>
2022-10-18 00:32:57 +0700

    menambahkan konstruktor di JPA

---
+++ src/main/java/a/ws/invetaris/ws.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package a.ws.invetaris;
+
+/**
+ *
+ * @author ERDA WYNE
+ */
+class ws {
+
+    static String a_learningapi_jar_0;
+    
+}
